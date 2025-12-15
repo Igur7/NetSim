bed04ee3987d9da652741d9946190711f21a3e70
Maciej Pałka <maciej.palka123@gmail.com>
2025-12-15 12:01:43 +0100

    added header files for storehouse

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -1,1 +1,16 @@
 #pragma once 
+
+#include "storage_types.hpp"
+#include <memory>
+
+class Storehouse{
+    public:
+        explicit Storehouse(std::unique_ptr<IPackageStockpile> stockpile = std::make_unique<PackageQueue>());
+        void ReceivePackage(Package&& package);
+        Package ReleasePackage();
+        bool Empty() const;
+
+    private:
+    std::unique_ptr<IPackageStockpile> stockpile_;
+
+}
